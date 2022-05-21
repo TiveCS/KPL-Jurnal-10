b44e94bb280c45e7f097d75796651e8bedc83ae7
TiveCS <creativecode.id@gmail.com>
2022-05-21 16:43:54 +0700

    penjelasan code

--- modul10_1302204090/CariNilaiPangkat.cs
+++ modul10_1302204090/CariNilaiPangkat.cs
@@ -10,21 +10,28 @@ namespace modul10_1302204090
 	{
 		public static int CariNilaiPangkat(int a, int b)
 		{
+			// Cek jika b == 0, maka return 1
 			if (b == 0) return 1;
+
+			// Cek jika b < 0, maka return -1
 			if (b < 0) return -1;
 
+			// Cek jika b > 10 atau a > 100, return -1
 			if (b > 10 || a > 100) return -2;
 
 
+			// Proses perhitungan pangkat
 			int result = 1;
 			for (int i = 0; i < b; i++)
 			{
 				try
 				{
+					// Menghitung sambil mengecek jika terjadi Exception
 					result = checked(result * a);
 				}
 				catch (OverflowException)
 				{
+					// Return -3 saat terjadi OverflowException (value lebih dari kapasitas)
 					return -3;
 				}
 
